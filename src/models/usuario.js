361b9d49dd3b2ee07d95580c15eb0a7bdfa757ba
JhonRamirez23 <jframirez074@soy.sena.edu.co>
2025-05-17 14:16:15 -0500

    Carga usuario
    Se arregla y se carga nuevamente el archivo

--- src/models/usuario.js
+++ src/models/usuario.js
@@ -1,19 +1,13 @@
 module.exports = (sequelize, DataTypes) => {
   return sequelize.define('Usuario', {
-    id_usuario: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    nombre: DataTypes.STRING,
-    correo: {
-      type: DataTypes.STRING,
-      unique: true
-    },
-    contraseña: DataTypes.STRING,
-    rol: DataTypes.STRING
+    id_usuario: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    nombre: { type: DataTypes.STRING},
+    email: { type: DataTypes.STRING, unique: true },
+    apellido: { type: DataTypes.STRING, allowNull: true },
+    password: { type: DataTypes.STRING, allowNull: false },
+    rol: { type: DataTypes.STRING }
   }, {
     tableName: 'usuarios',
-    timestamps: false
+    timestamps: true
   });
 };
