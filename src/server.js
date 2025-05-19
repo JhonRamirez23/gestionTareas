20747584a40d4621a9c48d34a0518bce1f539d4d
John Ramirez <131934814+JhonRamirez23@users.noreply.github.com>
2025-05-19 07:54:45 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const app = require('./app');
 const sequelize = require('./config/database');
 const mongoose = require('./config/mongo');
 
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 8080;
 
 sequelize.authenticate()
   .then(() => {
