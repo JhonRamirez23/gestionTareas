0a48a2dd1338893f9bb5883200f37d37a85b067e
John Ramirez <131934814+JhonRamirez23@users.noreply.github.com>
2025-05-19 16:36:38 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,7 +10,8 @@ const taskLabel = require('./routes/label_task');
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/v1', (req, res) => { res.status(200}.json({ message: 'Listening API'});
+app.use('/)', (req, res) => {res.send("Escuchando API")};
+app.use('/api/v1', (req, res) => { res.estatus(200).json({ status: 'ok'});
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/tasks', taskRoutes);
 app.use('/api/v1/projects', projectRoutes);
