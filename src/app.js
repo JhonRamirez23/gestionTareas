9e60459ee00d6c433680bc63ed3fd4b6bd5dcfd0
John Ramirez <131934814+JhonRamirez23@users.noreply.github.com>
2025-05-19 16:43:46 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,8 +10,7 @@ const taskLabel = require('./routes/label_task');
 app.use(cors());
 app.use(express.json());
 
-app.use('/', (req, res) => { res.send("Escuchando API")});
-app.use('/api/v1', (req, res) => { res.estatus(200).json({ status: 'ok'});
+app.use('/', (req, res) => { res.send("Servidor corriendo en Railway")});
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/tasks', taskRoutes);
 app.use('/api/v1/projects', projectRoutes);
